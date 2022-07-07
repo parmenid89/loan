86b8facf2ecb40393f6e7f8729bc193bcae8a4b0
Daniil <Daniil.postoyuk@gmail.com>
2022-07-07 21:54:22 +0300

    added class Forms

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import MainSlider from './modules/slider/slider-main';
 import MiniSlider from './modules/slider/slider-mini';
 import VideoPlayer from './modules/playVideo';
+import Difference from './modules/difference';
+import Form from './modules/forms';
 
 window.addEventListener('DOMContentLoaded', () =>{
     const slider = new MainSlider({btns: '.next', container: '.page'});
@@ -35,5 +37,9 @@ window.addEventListener('DOMContentLoaded', () =>{
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
+
+    new Form('.form').init();
 
 });
